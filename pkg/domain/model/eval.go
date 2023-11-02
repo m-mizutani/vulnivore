122ac6ff757f9d6bfbcb3fe06e3e2c0b09a79a11
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2023-11-03 08:40:17 +0900

    Enable Rego policy for Sarif handler

--- pkg/domain/model/eval.go
+++ pkg/domain/model/eval.go
@@ -1,17 +1,20 @@
 package model
 
-import "github.com/aquasecurity/trivy/pkg/types"
-
-type EvalInputTrivyVuln struct {
-	Metadata *types.Metadata
-	Result   *types.Result
-	Vuln     *types.DetectedVulnerability
-}
+import (
+	"github.com/aquasecurity/trivy/pkg/types"
+	"github.com/securego/gosec/v2/report/sarif"
+)
 
 type EvalOutput struct {
 	Action    string   `json:"action"`
 	Labels    []string `json:"labels"`
 	Assignees []string `json:"assignees"`
+}
+
+type EvalInputTrivyVuln struct {
+	Metadata *types.Metadata
+	Result   *types.Result
+	Vuln     *types.DetectedVulnerability
 }
 
 func NewEvalInputTrivyVuln(report types.Report, result types.Result, vuln types.DetectedVulnerability) *EvalInputTrivyVuln {
@@ -24,5 +27,21 @@ func NewEvalInputTrivyVuln(report types.Report, result types.Result, vuln types.
 		Metadata: &report.Metadata,
 		Result:   &tmpResult,
 		Vuln:     &vuln,
+	}
+}
+
+type EvalInputSarif struct {
+	Report   sarif.Report
+	Run      sarif.Run
+	Result   sarif.Result
+	Location sarif.Location
+}
+
+func NewEvalInputSarif(report sarif.Report, run sarif.Run, result sarif.Result, loc sarif.Location) *EvalInputSarif {
+	return &EvalInputSarif{
+		Report:   report,
+		Run:      run,
+		Result:   result,
+		Location: loc,
 	}
 }
